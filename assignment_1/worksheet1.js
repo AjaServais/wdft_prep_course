ea99fef9b71f37d9b099ba3681dd8d5fe50e9d81
Aja Servais <ajaservais249@gmail.com>
2017-03-15 14:53:12 -0400

    n

--- assignment_1/worksheet1.js
+++ assignment_1/worksheet1.js
@@ -6,6 +6,24 @@ var x = 1125;
 var y = 2;
 console.log (x % y);
 
+var a = true;
+var b = false;
+var c = true;
+console.log (a || b || c);
+
+var a = true;
+var b = false;
+var c = true;
+console.log (a && b && c);
+
+//Compute 10 - 3 * 5/4 + 202 % 3 and assign it to the variable answer1.
+//Compute 10 - (3 * (5/4) + (202 % 3)) and assign it to the variable answer2.
+//Print both of these answers. Why do you think they are different? Research the order of operations for JavaScript.  
+
+var answer1 = 10 - 3 * 5/4 + 202 % 3;
+console.log (answer1);
+
+
 
 
 
