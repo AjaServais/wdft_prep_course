c27f1e860fcfd878119035d0d58c07a5b5747a0a
Aja Servais <ajaservais249@gmail.com>
2017-03-16 11:01:56 -0400

    c

--- assignment_1/worksheet2.js
+++ assignment_1/worksheet2.js
@@ -5,18 +5,20 @@ function ask(){
     var choice = Math.floor(Math.random() * numberofreplys +1);
     var reply = 'You need to make some options using if/else'
 
-if (choice == 2) {
-    reply = "it will be a great day";
-} else {
-    reply = "does not look so good";
-}
+
     /*
     write conditional logic here using if, else if, and else
 
 
     */
-
-    console.log(choice);
+if (choice === 2) {
+    reply = "it will be a great day";
+} else if (choice === 4) {
+    reply = "lucky day";
+} else {
+    reply = "You need to make some options using if/else";
 }
-
+    console.log(reply);
+}
 ask();
+
