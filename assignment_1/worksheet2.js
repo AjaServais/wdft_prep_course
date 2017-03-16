4b51d05c045a03a22785819e59d4b93987a7d6a0
Aja Servais <ajaservais249@gmail.com>
2017-03-16 12:08:29 -0400

    n

--- assignment_1/worksheet2.js
+++ assignment_1/worksheet2.js
@@ -1,6 +1,6 @@
 //magic eightball
 
-var numberofreplys = 2;
+var numberofreplys = 6;
 function ask(){
     var choice = Math.floor(Math.random() * numberofreplys +1);
     var reply = 'You need to make some options using if/else'
@@ -13,7 +13,7 @@ function ask(){
     */
 if (choice === 2) {
     reply = "it will be a great day";
-} else if (choice === 4) {
+} else if (choice === 5) {
     reply = "lucky day";
 } else {
     reply = "You need to make some options using if/else";
@@ -22,3 +22,21 @@ if (choice === 2) {
 }
 ask();
 
+
+{function fizzBuzz(){
+	var output;
+	for(var i=1;i<=100;i++){
+		output = '';
+		if(i%3 === 0){
+			output ='Fizz';
+		} 
+		if(i%5 === 0){
+			output ='Buzz';
+		} 
+		if(i%3 !== 0 && i%5 !== 0) {
+			output+=i;
+        
+		} else {
+		console.log(i);
+	}
+    }
--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,6 +2,10 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
+for (var i = 1; i < 101; i++) {
+    console.log(i);
+
+}
 
 
 
@@ -17,3 +21,13 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+var numbers = [];
+
+for (var i=1;i<8;i++){ 
+numbers.push(i);
+console.log(numbers);
+
+
+ 
+    //use the array push() method here.
+}
